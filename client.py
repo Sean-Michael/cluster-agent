e0abb5d98169cd43f9e4364b2add02fd8182f815
Sean-Michael <seanm.riesterer@gmail.com>
2025-12-19 15:43:16 -0800

    fixing some f strings and type hints, also using my custom openaitool basemodel for validation of format function..

--- client.py
+++ client.py
@@ -12,6 +12,7 @@ import asyncio
 import logging
 logger = logging.getLogger(__name__)
 from mcp import ClientSession, StdioServerParameters
+from mcp.types import Tool as MCPTool
 from mcp.client.stdio import stdio_client
 from pydantic import BaseModel
 
@@ -22,11 +23,11 @@ class OpenAITool(BaseModel):
     function: dict
 
 
-async def get_kubectl_tools(server_params: StdioServerParameters) -> list | None:
+async def get_kubectl_tools(server_params: StdioServerParameters) -> list[MCPTool] | None:
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
             await session.initialize()
-            logger.info("Connected to MCP server with params {server_params}\n")
+            logger.info(f"Connected to MCP server with params {server_params}\n")
 
             try: 
                 response = await session.list_tools()
@@ -37,22 +38,21 @@ async def get_kubectl_tools(server_params: StdioServerParameters) -> list | None
                     logger.debug(f"     Schema: {tool.inputSchema}")
                 return response.tools
             except Exception as e:
-                logger.error("Exception caught : {e}")
+                logger.error(f"Exception caught : {e}")
                 return None
 
 
-def format_tools(mcp_tools: list) -> list[dict]:
+def format_tools(mcp_tools: list[MCPTool]) -> list[OpenAITool]:
     formatted_tools = []
     for tool in mcp_tools:
         try:
-            formatted_tools.append({
-                "type": "function",
-                "function": {
+            formatted_tools.append(OpenAITool(
+                function = {
                     "name": tool.name,
                     "description": tool.description or "",
                     "parameters": tool.inputSchema
                 }
-            })
+            ))
             logger.info(f"Formatted: {tool.name} into OpenAI/Ollama compatible format")
         except Exception as e:
             logger.error(f"Exception formatting tool {tool.name}: {e}")
@@ -70,7 +70,7 @@ async def main():
 
     kubectl_tools = await get_kubectl_tools(server_params)
     fmt_kubectl_tools = format_tools(kubectl_tools)
-    logger.info(f"Formatted Tools:\n{list(t['function'].get('name') for t in fmt_kubectl_tools)}")
+    logger.info(f"Formatted Tools:\n{list(t.function.get('name') for t in fmt_kubectl_tools)}")
 
 if __name__ == "__main__":
     asyncio.run(main())
