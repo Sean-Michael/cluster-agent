6423226037c1074bb0ba7ace6500c80cbeca7cf8
Sean-Michael <seanm.riesterer@gmail.com>
2025-12-19 16:01:47 -0800

    basic tool calling with chat ollama

--- client.py
+++ client.py
@@ -15,15 +15,17 @@ from mcp import ClientSession, StdioServerParameters
 from mcp.types import Tool as MCPTool
 from mcp.client.stdio import stdio_client
 from pydantic import BaseModel
+import ollama
 
 
 class OpenAITool(BaseModel):
-    """An OpenAI/Ollama formatted tool from an MCP server"""
+    """An OpenAI/Ollama formatted tool Object"""
     type: str = "function"
     function: dict
 
 
 async def get_kubectl_tools(server_params: StdioServerParameters) -> list[MCPTool] | None:
+    """Uses MCPClient to list_tools(), returns a list MCPTool objects."""
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
             await session.initialize()
@@ -43,6 +45,7 @@ async def get_kubectl_tools(server_params: StdioServerParameters) -> list[MCPToo
 
 
 def format_tools(mcp_tools: list[MCPTool]) -> list[OpenAITool]:
+    """Converts an MCPTool into an OpenAITool, returns list of OpenAITool objects."""
     formatted_tools = []
     for tool in mcp_tools:
         try:
@@ -60,6 +63,19 @@ def format_tools(mcp_tools: list[MCPTool]) -> list[OpenAITool]:
     return formatted_tools
 
 
+def chat_with_tool(model: str, messages: dict[str], tools: list[OpenAITool]) -> dict | None:
+    try:
+        response = ollama.chat(
+            model=model,
+            messages=messages,
+            tools = tools
+        )
+        return response
+    except Exception as e:
+        logger.error(f"Exception in Ollama chat: {e}")
+        return None
+
+
 async def main():
     logging.basicConfig(level=logging.INFO)
 
@@ -69,8 +85,11 @@ async def main():
     )
 
     kubectl_tools = await get_kubectl_tools(server_params)
-    fmt_kubectl_tools = format_tools(kubectl_tools)
-    logger.info(f"Formatted Tools:\n{list(t.function.get('name') for t in fmt_kubectl_tools)}")
+    kubectl_tools_dict = [t.model_dump() for t in format_tools(kubectl_tools)]
+
+    messages = [{"role": "user", "content": "Show me what nodes are in my kubernetes cluster"}]
+    test_response = chat_with_tool("mistral-nemo:latest", messages, kubectl_tools_dict)
+    print(test_response)
 
 if __name__ == "__main__":
     asyncio.run(main())
